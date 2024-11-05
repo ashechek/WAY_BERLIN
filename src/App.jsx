8e10d78f4ff562f58deff642e78b62499cd8a15f
ashechek <ashechek3@gmail.com>
2024-11-05 21:48:29 +0200

    add schematics cards

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,16 @@
 import './App.css'
 import Links from "./componenst/Links/Links.jsx"
 import Overview from "./componenst/Overview/Overview.jsx"
+import Staff from './componenst/Staff/Staff.jsx'
+
+
 const App = () => {
   return (
     <div>
       <h1>MENSCH</h1>
       <h2 className='titleText'>Dein bester Salon</h2>
       <Overview />
+      <Staff />
       <Links />
     </div >
   )
