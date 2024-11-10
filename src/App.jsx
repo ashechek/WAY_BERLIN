bf65698146d002e572a08a89af5e262bcbab378b
ashechek <ashechek3@gmail.com>
2024-11-11 00:39:34 +0200

    REBUILD

--- src/App.jsx
+++ src/App.jsx
@@ -1,20 +1,18 @@
 import './App.css'
-import Links from "./componenst/Links/Links.jsx"
-import Overview from "./componenst/Overview/Overview.jsx"
-import Staff from './componenst/Staff/Staff.jsx'
 
+import React from 'react'
 
 const App = () => {
   return (
     <div>
       <h1>MENSCH</h1>
-      <h2 className='titleText'>Dein bester Salon</h2>
-      <Overview />
-      <Staff />
-      <Links />
-    </div >
+      <h2>Text example 1 to 1</h2>
+      <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quos impedit,
+        reprehenderit magni tempore sequi alias at optio nobis laudantium
+        dolorem quibusdam esse, labore sint dolor expedita, odit
+        voluptas consectetur fugiat.</p>
+    </div>
   )
 }
-
 
 export default App
