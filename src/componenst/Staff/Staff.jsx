c5b682ac7b4d84758b199faaf2e5e8583baeb118
ashechek <ashechek3@gmail.com>
2024-11-10 20:05:27 +0200

    remove cards

--- src/componenst/Staff/Staff.jsx
+++ src/componenst/Staff/Staff.jsx
@@ -4,26 +4,8 @@ import s from "./Staff.module.css"
 
 const Staff = () => {
     return (
-        <div className={s.wrapper}>
-            <div className={s.staffBox}>
-                <h2 className={s.titleText}>Susanna Mollie</h2>
-                <p className={s.text}>Alle Menschen sind frei und gleich an Würde und Rechten geboren.
-                    Alle Menschen sind frei und gleich an Würde und Rechten geboren.
-                    Alle Menschen sind frei und gleich an Würde und Rechten geboren.
-                    Alle Menschen sind frei und gleich an Würde und Rechten geboren.
-                    Alle Menschen sind frei und gleich an Würde und Rechten geboren.
-                </p>
-                <img src="/src/assets/Images/SK.png" alt="Susanna Mollie" className={s.staffImage} />
-                <img />
-            </div>
-            <div className={s.staffBox}>
-                <h2 className={s.titleText}>Ben Moritz</h2>
-                <p className={s.text}>Alle Menschen sind frei und gleich an Würde und Rechten geboren.
-                    Alle Menschen sind frei und gleich an Würde und Rechten geboren.
-                    Alle Menschen sind frei und gleich an Würde und Rechten geboren.
-                </p>
-                <img src="/src/assets/Images/SK.png" alt="Susanna Mollie" className={s.staffImage} />
-                <img /></div>
+        <div>
+
         </div>
     )
 }
