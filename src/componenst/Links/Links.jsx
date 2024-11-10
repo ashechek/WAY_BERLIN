b5eb911c12849836c60d97b9698f60b6a5818538
ashechek <ashechek3@gmail.com>
2024-11-10 20:43:22 +0200

    a

--- src/componenst/Links/Links.jsx
+++ src/componenst/Links/Links.jsx
@@ -1,8 +1,9 @@
 import React from 'react'
 import s from "./Links.module.css"
+
 const Links = () => {
     return (
-        <div>
+        <div className={s.linksWrapper}>
             <a className={s.link1} href='https://www.instagram.com/svitlana_stylist_berlin' target="_blank" rel="noopener noreferrer">Instagram</a>
         </div >
     )
