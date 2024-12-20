f268e8519008b6dd92ab9bd97824ba303b20dfd2
ashechek <ashechek3@gmail.com>
2024-12-20 23:10:16 +0200

    1

--- src/components/About/About.jsx
+++ src/components/About/About.jsx
@@ -3,7 +3,7 @@ import s from "./About.module.css"
 const About = () => {
     return (
         <div>
-            <h1>WAY</h1>
+            <h1>WAY!</h1>
             <h2>BERLIN</h2>
             <p>Ein neues Wort in deinem Stil.</p>
         </div>
