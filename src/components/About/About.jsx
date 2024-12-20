430b535455cbc4b9a9375882b6d7fe9486f0b080
ashechek <ashechek3@gmail.com>
2024-12-20 23:27:49 +0200

    0.07

--- src/components/About/About.jsx
+++ src/components/About/About.jsx
@@ -3,10 +3,10 @@ import s from "./About.module.css"
 const About = () => {
     return (
         <div>
-            <h1>WAY!</h1>
-            <h2>BERLIN</h2>
-            <p>Ein neues Wort in deinem Stil.</p>
-            <p>V0.06</p>
+            <h1 className={s.title}>WAY!</h1>
+            <h2 className={s.subtitle}>BERLIN</h2>
+            <p className={s.text}>Ein neues Wort in deinem Stil.</p>
+            <p className={s.text}>V0.07</p>
         </div>
     )
 }
